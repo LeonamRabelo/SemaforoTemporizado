f12b1043602a798b2e145d4f025c8802b8fb8739
Leonam Rabelo <rabeloleonam@gmail.com>
2025-02-02 01:11:52 -0300

    Adicionando comentarios no código

--- SemaforoTemporizado.c
+++ SemaforoTemporizado.c
@@ -2,18 +2,19 @@
 #include "pico/stdlib.h"
 #include "hardware/timer.h" //Inclui a biblioteca para gerenciamento de temporizadores de hardware.
 
+//Definição dos pinos dos leds
 #define LED_PIN_RED 11
 #define LED_PIN_YELLOW 12
 #define LED_PIN_GREEN 13
 
-//Funcao para inicializar os pinos
+//Funcao para inicializar os pinos e comunicação
 void InicizalizaGPIO(){
-    stdio_init_all();    
+    stdio_init_all();     //Inicializa a comunicação serial, permitindo o uso de funções como printf.
 
     //Inicializa pinos dos leds
     gpio_init(LED_PIN_RED);
     gpio_set_dir(LED_PIN_RED, GPIO_OUT);
-    gpio_put(LED_PIN_RED, 1);
+    gpio_put(LED_PIN_RED, 1);   //Inicializo com apenas o led vermelho ligado, como pedido na tarefas
     
     gpio_init(LED_PIN_YELLOW);
     gpio_set_dir(LED_PIN_YELLOW, GPIO_OUT);
@@ -24,18 +25,24 @@ void InicizalizaGPIO(){
     gpio_put(LED_PIN_GREEN, 0);
 }
 
+//Funcao a ser chamada no temporizador
 bool repeating_timer_callback(struct repeating_timer *t){
-  if(gpio_get(LED_PIN_RED)==1 && gpio_get(LED_PIN_YELLOW)==0 && gpio_get(LED_PIN_GREEN)==0){
+    //Se apenas o led vermelho estiver ativo, desativa ele e passa a ativar o amarelo
+    if(gpio_get(LED_PIN_RED)==1 && gpio_get(LED_PIN_YELLOW)==0 && gpio_get(LED_PIN_GREEN)==0){
     gpio_put(LED_PIN_RED, 0);
     gpio_put(LED_PIN_YELLOW, 1);
-  }else if(gpio_get(LED_PIN_RED)==0 && gpio_get(LED_PIN_YELLOW)==1 && gpio_get(LED_PIN_GREEN)==0){
+    }
+    //Se apenas o led amarelo estiver ativo, desativa ele e passa a ativar o verde
+    else if(gpio_get(LED_PIN_RED)==0 && gpio_get(LED_PIN_YELLOW)==1 && gpio_get(LED_PIN_GREEN)==0){
     gpio_put(LED_PIN_YELLOW, 0);
     gpio_put(LED_PIN_GREEN, 1);
-  }else if(gpio_get(LED_PIN_RED)==0 && gpio_get(LED_PIN_YELLOW)==0 && gpio_get(LED_PIN_GREEN)==1){
+    }
+    //Se apenas o led verde estiver ativo, desativa ele e passa a ativar o vermelho
+    else if(gpio_get(LED_PIN_RED)==0 && gpio_get(LED_PIN_YELLOW)==0 && gpio_get(LED_PIN_GREEN)==1){
     gpio_put(LED_PIN_GREEN, 0);
     gpio_put(LED_PIN_RED, 1);
-  }
-  return true;
+    }
+return true;
 }
 
 int main(){
@@ -47,15 +54,16 @@ int main(){
     //Configura o temporizador para chamar a função de callback a cada 3 segundos.
     add_repeating_timer_ms(3000, repeating_timer_callback, NULL, &timer);
 
+    //Laço de repetição principal
     while(true){
-        if(gpio_get(LED_PIN_RED)==1){
+        if(gpio_get(LED_PIN_RED)==1){   //Verifica se o led vermelho está aceso
             printf("PARADA OBRIGATÓRIA!!!");
-        }else if(gpio_get(LED_PIN_YELLOW)==1){
+        }else if(gpio_get(LED_PIN_YELLOW)==1){  //Verifica se o led amarelo está aceso
             printf("SINAL AMARELO, MUITA ATENÇÃO!!!");
-        }else if(gpio_get(LED_PIN_GREEN)==1){
+        }else if(gpio_get(LED_PIN_GREEN)==1){   //Verifica se o led verde está aceso
             printf("SINAL ABERTO, PROSSIGA!");
         }
-        sleep_ms(1000);
+        sleep_ms(1000); //Intervalo de 1 segundo entre cada verificação para imprimir os alertas
     }
   return 0;
 }
